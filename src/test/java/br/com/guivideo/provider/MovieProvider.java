9eb3f147f009d7208fe63097286a79c4bed58509
rodrigosv91 <rodrigosv91@gmail.com>
2023-05-30 17:44:42 -0300

    refactor: atualiza movie provider para execução dos testes com sucesso

--- src/test/java/br/com/guivideo/provider/MovieProvider.java
+++ src/test/java/br/com/guivideo/provider/MovieProvider.java
@@ -8,6 +8,6 @@ public class MovieProvider {
     public static final Movie MOVIE_A = new Movie("Filme A");
     public static final Movie MOVIE_A_SALVO = new Movie(1L,"Filme A");
     public static final MovieResponse MOVIE_RESPONSE_A = new MovieResponse("Filme A");
-    public static final String TITLE_MOVIE_A = "MOVIE_RESPONSE_A.getTitle()";
+    public static final String TITLE_MOVIE_A = MOVIE_RESPONSE_A.getTitle();
 
 }
