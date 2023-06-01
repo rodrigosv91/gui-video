f3f0c14b26f0dc03b4016dc25594fb01504d7db6
rodrigosv91 <rodrigosv91@gmail.com>
2023-06-01 11:23:53 -0300

    build: altera workflow

--- src/main/java/br/com/guivideo/api/v1/dto/response/MovieResponse.java
+++ src/main/java/br/com/guivideo/api/v1/dto/response/MovieResponse.java
@@ -2,11 +2,13 @@ package br.com.guivideo.api.v1.dto.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class MovieResponse {
     private String title;
 }
